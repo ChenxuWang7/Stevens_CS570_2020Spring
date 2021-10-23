4dd46b0d3df8d571eb44de1d59b64e5b69ea2c90
ChenxuWang7 <80286081+ChenxuWang7@users.noreply.github.com>
2021-10-23 01:24:36 -0400

    Create GridColors.java

---
+++ HW4/GridColors.java
@@ -0,0 +1,23 @@
+/**
+ * Student Name: Chenxu Wang
+ * ID: 10457625
+ * Course Name: CS 570
+ * Homework Assignment 4 
+ * */
+package Maze;
+
+import java.awt.Color;
+
+/**
+ *  An interface for colors
+ *@author Koffman and Wolfgang
+ */
+public interface GridColors {
+
+    Color PATH = Color.green;
+    Color BACKGROUND = Color.white;
+    Color NON_BACKGROUND = Color.red;
+    Color ABNORMAL = NON_BACKGROUND;
+    Color TEMPORARY = Color.black;
+}
+/*</exercise>*/
